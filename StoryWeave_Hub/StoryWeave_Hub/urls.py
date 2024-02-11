34d88a5460aa8ef3cf84c6ba7b51c6f6dea5c160
Ekansh38 <goenka.ekansh@gmail.com>
2024-02-11 10:21:12 +0800

    styling changes

--- StoryWeave_Hub/StoryWeave_Hub/urls.py
+++ StoryWeave_Hub/StoryWeave_Hub/urls.py
@@ -1,30 +1,17 @@
-"""
-URL configuration for googleauth project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 from hub import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('signup/', views.signup_view, name='signup'),
-    path('social-auth/', include('social_django.urls', namespace='social')),
-
-    path('', views.index, name='index'),
-    path('main-story/', views.main_story, name='main_story'),
+    path("admin/", admin.site.urls),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("signup/", views.signup_view, name="signup"),
+    path("social-auth/", include("social_django.urls", namespace="social")),
+    path("", views.index, name="index"),
+    path("main-story/", views.main_story, name="main_story"),
+    path("create-branch/", views.create_branch, name="create_branch"),
+    path("view_branches", views.view_branches, name="view_branches"),
+    path("remember/", views.remember, name="remember"),
+    path("un-remember/", views.un_remember, name="un_remember"),
 ]
--- StoryWeave_Hub/hub/views.py
+++ StoryWeave_Hub/hub/views.py
@@ -1,79 +1,112 @@
-from django.shortcuts import render
-from django.contrib.auth import logout, authenticate, login
-from django.http import HttpResponseRedirect
-from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
 
-from .models import MainStory, Branch, Vote
+from .models import Branch, MainStory
 
 
 def index(request):
     if request.user.is_authenticated:
-        return render(request, 'hub/index_in.html', {
-            "user": request.user
-            })
-    return render(request, 'hub/index_out.html')
+        return render(request, "hub/index_in.html", {"user": request.user})
+    return render(request, "hub/index_out.html")
 
 
 def login_view(request):
     if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(
-            request,
-            username=username,
-            password=password
-        )
+        username = request.POST["username"]
+        password = request.POST["password"]
+        remember = request.POST.get("remember", None)
+        if remember == None:
+            remember = False
+        elif remember == "on":
+            remember = True
+
+        if remember:
+            request.session.set_expiry(604800)
+        else:
+            request.session.set_expiry(0)
+
+        user = authenticate(request, username=username, password=password)
 
         if user is not None:
             login(request, user)
-            return HttpResponseRedirect(reverse('index'))
+            return HttpResponseRedirect(reverse("index"))
 
-    return render(request, 'hub/login.html')
+    request.session.set_expiry(0)
+    return render(request, "hub/login.html")
 
 
 def signup_view(request):
     if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        password_confirm = request.POST['password_confirm']
-        list_of_users = User.objects.all().values_list('username', flat=True)
+        username = request.POST["username"]
+        password = request.POST["password"]
+        password_confirm = request.POST["password_confirm"]
+        list_of_users = User.objects.all().values_list("username", flat=True)
 
         if username in list_of_users:
-            return render(request, 'hub/signup.html', {
-                "message": "Username Already Exists!"
-                })
+            return render(
+                request, "hub/signup.html", {"message": "Username Already Exists!"}
+            )
 
         if password != password_confirm:
-            return render(request, 'hub/signup.html', {
-                "message": "Passwords Must Match!"
-                })
+            return render(
+                request, "hub/signup.html", {"message": "Passwords Must Match!"}
+            )
 
         user = User.objects.create_user(username=username, password=password)
         user.save()
-        user.backend = 'django.contrib.auth.backends.ModelBackend'
+        user.backend = "django.contrib.auth.backends.ModelBackend"
         login(request, user)
-        return HttpResponseRedirect(reverse('index'))
+        return HttpResponseRedirect(reverse("index"))
 
-    return render(request, 'hub/signup.html')
+    return render(request, "hub/signup.html")
 
 
+@login_required
 def logout_view(request):
     logout(request)
-    return HttpResponseRedirect(reverse('index'))
+    return HttpResponseRedirect(reverse("index"))
 
 
+@login_required
 def main_story(request):
-    branches = MainStory.objects.all().order_by('id') #type: ignore 
+    branches = MainStory.objects.all().order_by("id")
     story_text = ""
 
     for branch in branches:
         story_text += branch.branch.text_data
 
-    return render(request, 'hub/main_story.html', {
-        "story_text": story_text
-        })
+    return render(request, "hub/main_story.html", {"story_text": story_text})
 
 
+@login_required
+def create_branch(request):
+    if request.method == "POST":
+        content = request.POST["content"]
+        title = request.POST["title"]
+        branch = Branch(title=title, text_data=content, author=request.user)
+        branch.save()
+        return HttpResponseRedirect(reverse("index"))
+
+    return render(request, "hub/create_branch.html")
 
 
+@login_required
+def view_branches(request):
+    branches = Branch.objects.filter(author=request.user)
+    return render(request, "hub/view_branches.html", {"branches": branches})
+
+
+def remember(request):
+    print("remembered")
+    request.session.set_expiry(604800)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def un_remember(request):
+    print("un-remembered")
+    request.session.set_expiry(0)
+    return HttpResponseRedirect(reverse("index"))
